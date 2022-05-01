c89d65a03698ea68fbbd6d6669989e9d81696a35
ltestut <laurent.testut@univ-lr.fr>
2022-05-01 12:18:06 +0200

    update display

--- savon.py
+++ savon.py
@@ -3,9 +3,20 @@ from pathlib import Path
 from configparser import ConfigParser
 import numpy as np
 import pandas as pd
+import json
+from datetime import date
 
 recette = Path.cwd() / 'recette.ini'
+moules_file = Path().cwd() / 'moules.json'
+with open(moules_file, 'r') as mf:
+    moules = json.load(mf)
+
+
 #recette = Path(r'C:\Users\ltestut\PycharmProjects\mishi\recette.ini')
+
+def get_ptf(config,moules):
+    "recupere le poids total du savon du fichier moules"
+    return moules[config['IDENTITE']['moule']]['poids_savon']
 
 def volume_moule(config):
     "calcul du volume du moule en cm3"
@@ -20,15 +31,16 @@ def verification_huiles(config):
         sum+=int(config['HUILES'][huile])
     return True if sum == 100 else False
 
-def somme_liquides(config):
+def somme_liquides(config,verbose=False):
     "calcul de la somme des liquides en pourcentage "
     sum = 0
     for i in config['LIQUIDES']:
-        print(f" ... {i} = {config['LIQUIDES'][i]} %")
+        if verbose :
+            print(f"       {i} = {config['LIQUIDES'][i]} %")
         sum+=float(config['LIQUIDES'][i])
     return sum
 
-def calcul_pth(config):
+def get_pth(config):
     "Calcul du poids total des huiles à partir du poids total final"
     ptf = volume_moule(config)   #poids total final = volume du moule
     surgras = float(config['IDENTITE']['surgras'])/100. # en pourcentage
@@ -38,7 +50,7 @@ def calcul_pth(config):
 
 def calcul_soude(config,surgras=None): #TODO a refaire il est faut
     "calcul du poid total de la soude "
-    pth = calcul_pth(config)
+    pth = get_pth(config)
     huiles = config['HUILES']
     if surgras is None :
         surgras = float(config['IDENTITE']['surgras'])
@@ -61,7 +73,7 @@ def update_recette(config):
     config['QUANTITE']['Poid Total Savon  ']  = f"{round(volume_moule(config))} gr # 1 litre = 1 kg"
     for x in liquides:
         config['QUANTITE'][x] = f"{round(float(liquides[x]) / 100. * pth)} gr"
-    config['QUANTITE']['Poid Total Huiles ']  = f"{round(calcul_pth(config))} gr "
+    config['QUANTITE']['Poid Total Huiles ']  = f"{round(get_pth(config))} gr "
     for x in huiles:
         config['QUANTITE'][x] = f"{round(float(huiles[x]) / 100. * pth)} gr"
     config['QUANTITE']['soude'] = f"{calcul_soude(config)} gr"
@@ -72,7 +84,7 @@ def update_recette(config):
 
 def verion_html(config):
     "ecriture dans un fichier pandas DataFrame puis dans un html"
-    pth = round(calcul_pth(config))
+    pth = round(get_pth(config))
     d={'PTS':[np.nan,round(volume_moule(config))],
        '#savons':[round(volume_moule(config))/120,np.nan],
        'PTH':[100,pth],
@@ -98,29 +110,37 @@ def verion_html(config):
     print(f"ecriture du fichier {output_name}")
     return df
 
-#%% lecture de la recette
+#%% lancement du programme
+# 1. lecture de la recette
 config=ConfigParser()
 if recette.exists() :
     print(f"lecture du fichier {recette}")
     config.read(recette)
-    config.filename = recette
-    huiles = config['HUILES']
-    liquides = config['LIQUIDES']
-    coef = config['COEF']
-    surgras = float(config['IDENTITE']['surgras'])
+    config.filename = recette #ajout du nom de fichier dans la parser
+    moule, surgras  = config['IDENTITE']['moule'],float(config['IDENTITE']['surgras'])
+    huiles, liquides, coef = config['HUILES'], config['LIQUIDES'], config['COEF']
+    volume = volume_moule(config)
+    ptf    = round(volume)
 else :
     print(f"le fichier {recette} n'existe pas")
 
+#%% 2. information
 #calcul du PTF à partir du volume du moule
-print(f" Volume du moule = {volume_moule(config):.1f} cm3 soit {volume_moule(config)/1000.:.3f} litre")
-ptf = volume_moule(config)
+print(f" Recapitulatif pour la recette {config['IDENTITE']['nom'].upper()} à {surgras}% de surgras du {date.today()}")
+print(f"    1. PTF theorique = {ptf} gr (cm3) pour un moule de dim :"
+      f" L={config['MOULE']['longueur']}cm/l={config['MOULE']['largeur']}cm/H={config['MOULE']['hauteur']}cm ")
+print(f"       PTF observee  = {get_ptf(config,moules)} gr pour le moule {moule}")
+print(f"           soit {moules[moule]['nbr_savons']} savons de {moules[moule]['poids_unite']} gr ")
+print(f"    2. Liquides contenu dans le savon ")
+pliq=somme_liquides(config,verbose=True)
+print(f"       Total Liquides {pliq} %")
 
 #verification de la quantite d'huile et calcul des quantites
 if not verification_huiles(config) :
     print(f" ATTENTION la somme des huiles ne fait pas 100 %")
 else :
-    print(f" Composition des huiles ")
-    pth  = calcul_pth(config)
+    print(f"   2.  Composition des huiles ")
+    pth  = get_pth(config)
     soude = 0
     for x in huiles:
         huile = float(huiles[x])/100.*pth #quantite d'huile en grammes
